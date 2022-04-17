4bdf7c1abbc85ae32d394d0fc1340c64ca6d58c7
jiangxd <jiangxd@runsa.cn>
2022-04-17 13:20:01 +0800

    chore: update

--- packages/all/index.js
+++ packages/all/index.js
@@ -1,8 +1,9 @@
 const { isPackageExists } = require('local-pkg')
 const { defineConfig } = require('eslint-define-config')
-const isVueExists = isPackageExists('vue')
 
+const isVueExists = isPackageExists('vue')
 const eslintList = ['@estjs/eslint-config-prettier']
+
 if (isVueExists) {
   const Vue = require('vue')
   eslintList.push(Vue.version.startsWith('2.') ? '@estjs/eslint-config-vue2' : '@estjs/eslint-config-vue')
