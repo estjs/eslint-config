6f266235174e4965b0878e349c9bc171cbbe31a6
jiangxd <jiangxd2016@gmail.com>
2022-04-21 13:10:53 +0800

    chore: delete prettier

--- packages/all/index.js
+++ packages/all/index.js
@@ -1,7 +1,7 @@
 const { isPackageExists } = require('local-pkg')
 const isVueExists = isPackageExists('vue')
 
-const eslintList = ['@estjs/eslint-config-prettier']
+const eslintList = []
 if (isVueExists) {
   const Vue = require('vue')
   eslintList.push(Vue.version.startsWith('2.') ? '@estjs/eslint-config-vue2' : '@estjs/eslint-config-vue')
