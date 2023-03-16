0d0f8159ff6c27a0ee3437b05504da6af2eea064
jiangxd <jiangxd2016@gmail.com>
2023-03-16 09:46:45 +0800

    chore: update react eslint config

--- packages/all/index.js
+++ packages/all/index.js
@@ -1,10 +1,13 @@
 const { isPackageExists } = require('local-pkg');
 const isVueExists = isPackageExists('vue');
+const isReactExists = isPackageExists('react');
 
 const eslintList = [];
 if (isVueExists) {
   const Vue = require('vue');
   eslintList.push(Vue.version.startsWith('2.') ? '@estjs/eslint-config-vue2' : '@estjs/eslint-config-vue');
+} else if (isReactExists) {
+  eslintList.push('@estjs/eslint-config-react');
 } else {
   eslintList.push('@estjs/eslint-config-ts');
 }
