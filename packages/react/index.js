82908854bf7544785c9a5feb9c6cffb1dc3949ad
jiangxd <jiangxd2016@gmail.com>
2023-03-16 10:19:38 +0800

    fix: fix react lint typesctript not exists

--- packages/react/index.js
+++ packages/react/index.js
@@ -1,37 +1,42 @@
+const isTsExists = isPackageExists('typescript');
+
+const eslintExtends =[
+	'plugin:react/recommended',
+	'plugin:react-hooks/recommended'
+];
+
+eslintExtends.push(isTsExists ? '@estjs/eslint-config-ts' : '@estjs/eslint-config-basic');
+
 module.exports = {
-  extends: [
-    '@estjs/eslint-config-ts',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended'
-  ],
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-  rules: {
-    'jsx-quotes': [
-      'error',
-      'prefer-double',
-    ],
-    'react/jsx-no-bind': ['error', {
-      allowArrowFunctions: true,
-      allowBind: false,
-      ignoreRefs: true
-    }],
-    'react/prop-types': 'off',
-    'react/destructuring-assignment': 0,
-    'react/no-unknown-property': 'error',
-    'react/no-unused-prop-types': 'error',
-    'react/no-did-update-set-state': 'error',
+	extends:eslintExtends,
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+	rules: {
+		'jsx-quotes': [
+			'error',
+			'prefer-double',
+		],
+		'react/jsx-no-bind': ['error', {
+			allowArrowFunctions: true,
+			allowBind: false,
+			ignoreRefs: true
+		}],
+		'react/prop-types': 'off',
+		'react/destructuring-assignment': 0,
+		'react/no-unknown-property': 'error',
+		'react/no-unused-prop-types': 'error',
+		'react/no-did-update-set-state': 'error',
 
-    'react/jsx-closing-tag-location': 0,
-    'react/jsx-filename-extension': 0,
-    'react/jsx-props-no-spreading': 0,
-    'react/jsx-first-prop-new-line': [2, 'multiline'],
+		'react/jsx-closing-tag-location': 0,
+		'react/jsx-filename-extension': 0,
+		'react/jsx-props-no-spreading': 0,
+		'react/jsx-first-prop-new-line': [2, 'multiline'],
 
-    'react/react-in-jsx-scope': 'error',
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-  },
+		'react/react-in-jsx-scope': 'error',
+		'react-hooks/rules-of-hooks': 'error',
+		'react-hooks/exhaustive-deps': 'warn',
+	},
 };
