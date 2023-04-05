b2ebef9b63b6ac443a2bf5abd4fd2c76e6ce0033
jiangxd <jiangxd2016@gmail.com>
2023-04-05 20:24:14 +0800

    feat: update eslint rule newline after import

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -151,6 +151,7 @@ module.exports = {
     'import/no-mutable-exports': 'error',
     'import/no-unresolved': 'off',
     'import/no-absolute-path': 'off',
+    'import/newline-after-import': ['error', { count: 1 }],
 
     // Common
     'semi': ['warn', 'always'],
