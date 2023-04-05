7a5cd66d92715a911a6d00f07de60a95541be03e
jiangxd <jiangxd2016@gmail.com>
2023-04-05 21:13:16 +0800

    feat: update unocss support

--- packages/all/index.js
+++ packages/all/index.js
@@ -1,15 +1,28 @@
 const { isPackageExists } = require('local-pkg');
+
 const isVueExists = isPackageExists('vue');
 const isReactExists = isPackageExists('react');
+const isTSExists = isPackageExists('typescript');
+const isUnoCssExists = isPackageExists("unocss")
 
 const eslintList = [];
 if (isVueExists) {
   const Vue = require('vue');
   eslintList.push(Vue.version.startsWith('2.') ? '@estjs/eslint-config-vue2' : '@estjs/eslint-config-vue');
-} else if (isReactExists) {
+}
+if (isReactExists) {
   eslintList.push('@estjs/eslint-config-react');
-} else {
+}
+
+if(isUnoCssExists){
+	eslintList.push("@unocss/eslint-config")
+}
+
+
+if (isTSExists) {
   eslintList.push('@estjs/eslint-config-ts');
+} else {
+  eslintList.push('@estjs/eslint-config-basic');
 }
 module.exports = {
   extends: eslintList,
