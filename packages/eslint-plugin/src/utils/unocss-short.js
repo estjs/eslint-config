1e9500c7a4ea4f274b8bbd067aac8e863df82cb7
jiangxd <jiangxd2016@gmail.com>
2023-04-08 13:14:11 +0800

    fix: fix unocss short

--- packages/eslint-plugin/src/utils/unocss-short.js
+++ packages/eslint-plugin/src/utils/unocss-short.js
@@ -1,6 +1,6 @@
 import { runAsWorker } from 'synckit';
 
-// support short token
+// 支持简写的 token
 const shortTokens = [
   'dark',
   'flex',
@@ -12,39 +12,36 @@ const shortTokens = [
 ];
 
 runAsWorker(async (classList) => {
-
   const used = [];
   const unused = [];
   const genrate = [];
 
-  for (const className1 of classList) {
-
-    if (used.includes(className1)) {
+  for (const className of classList) {
+    if (used.includes(className)) {
       continue;
     }
-    // support short token
+    // 支持简写的 token
     let token = '';
     const isShortToken = shortTokens.some((i) => {
-      if (className1.startsWith(i)) {
+      if (className.startsWith(i)) {
         token = i;
         return true;
       }
       return false;
     });
     if (isShortToken) {
-      used.push(className1);
-      // find equal class
-      const equalClass = classList.find(i => i.startsWith(token));
-      used.push(...equalClass);
+      used.push(className);
+      // 找到对应的 class
+      const equalClasses = classList.filter(i => i.startsWith(token));
+      equalClasses.forEach(equalClass => used.push(equalClass));
 
-      const gen = `${token}-(${equalClass.map(i=>i.replace(token, '')).join(' ')})`;
+      const gen = `${token}-(${equalClasses.map(i=>i.replace(token, '')).join(' ')})`;
 
       genrate.push(gen);
 
     } else {
-      unused.push(className1);
+      unused.push(className);
     }
-
   }
 
   return {
