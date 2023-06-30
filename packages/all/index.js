b999874dec5087aa34051ba452ea1ce82c2ee394
jiangxd <jiangxd2016@gmail.com>
2023-06-30 10:54:12 +0800

    feat: delete file name & prettier

--- packages/all/index.js
+++ packages/all/index.js
@@ -1,26 +1,34 @@
-const { isPackageExists } = require("local-pkg");
+/*
+ * @Author: jiangxd<jiangxd2016@gmail.com>
+ * @Date: 2023-06-30 10:51:38
+ * @LastEditTime: 2023-06-30 10:52:11
+ * @LastEditors: jiangxd<jiangxd2016@gmail.com>
+ * @Description:
+ * @FilePath: /eslint-config/packages/all/index.js
+ */
+const { isPackageExists } = require('local-pkg');
 
-const isVueExists = isPackageExists("vue");
-const isReactExists = isPackageExists("react");
-const isTSExists = isPackageExists("typescript");
-const isAtomicExists = isPackageExists("unocss") || isPackageExists("tailwindcss") || isPackageExists("windicss");
+const isVueExists = isPackageExists('vue');
+const isReactExists = isPackageExists('react');
+const isTSExists = isPackageExists('typescript');
+const isAtomicExists = isPackageExists('unocss') || isPackageExists('tailwindcss') || isPackageExists('windicss');
 
 const eslintExtends = [];
 if (isVueExists) {
-	const Vue = require("vue");
-	eslintExtends.push(Vue.version.startsWith("2.") ? "@estjs/eslint-config-vue2" : "@estjs/eslint-config-vue");
+  const Vue = require('vue');
+  eslintExtends.push(Vue.version.startsWith('2.') ? '@estjs/eslint-config-vue2' : '@estjs/eslint-config-vue');
 } else if (isReactExists) {
-	eslintExtends.push("@estjs/eslint-config-react");
+  eslintExtends.push('@estjs/eslint-config-react');
 } else if (isTSExists) {
-	eslintExtends.push("@estjs/eslint-config-ts");
+  eslintExtends.push('@estjs/eslint-config-ts');
 } else {
-	eslintExtends.push("@estjs/eslint-config-basic");
+  eslintExtends.push('@estjs/eslint-config-basic');
 }
 
 if (isAtomicExists) {
-	eslintExtends.push("@unocss");
+  eslintExtends.push('@unocss');
 }
 
 module.exports = {
-	extends: eslintExtends,
+  extends: eslintExtends,
 };
--- packages/basic/CHANGELOG.md
+++ packages/basic/CHANGELOG.md
@@ -5,37 +5,70 @@ See [Conventional Commits](https://conventionalcommits.org) for commit guideline
 
 # [0.2.0](https://github.com/estjs/eslint-config/compare/v0.1.8...v0.2.0) (2022-04-18)
 
+
 ### Features
 
-- update rule ([c834ac7](https://github.com/estjs/eslint-config/commit/c834ac739de0d49a984c2162ec137cc9edd5a1d7))
+* update rule ([c834ac7](https://github.com/estjs/eslint-config/commit/c834ac739de0d49a984c2162ec137cc9edd5a1d7))
+
+
+
+
 
 ## [0.1.7](https://github.com/estjs/eslint-config/compare/v0.1.6...v0.1.7) (2022-04-18)
 
 **Note:** Version bump only for package @estjs/eslint-config-basic
 
+
+
+
+
 ## [0.1.6](https://github.com/estjs/eslint-config/compare/v0.1.5...v0.1.6) (2022-04-17)
 
 **Note:** Version bump only for package @estjs/eslint-config-basic
+
+
+
+
 
 ## [0.0.7](https://github.com/ventjs/eslint-config/compare/v0.0.6...v0.0.7) (2022-04-14)
 
 **Note:** Version bump only for package @estjs/eslint-config-basic
 
+
+
+
+
 ## [0.0.6](https://github.com/ventjs/eslint-config/compare/v0.0.5...v0.0.6) (2022-04-13)
 
 **Note:** Version bump only for package @estjs/eslint-config-basic
+
+
+
+
 
 ## [0.0.5](https://github.com/ventjs/eslint-config/compare/v0.0.4...v0.0.5) (2022-04-06)
 
 **Note:** Version bump only for package @estjs/eslint-config-basic
 
+
+
+
+
 ## [0.0.4](https://github.com/ventjs/eslint-config/compare/v0.0.3...v0.0.4) (2022-04-02)
 
 **Note:** Version bump only for package @estjs/eslint-config-basic
 
+
+
+
+
 ## [0.0.3](https://github.com/estjs/eslint-config/compare/v0.0.2...v0.0.3) (2022-03-31)
 
 **Note:** Version bump only for package @estjs/eslint-config-basic
+
+
+
+
 
 ## 0.0.2 (2022-03-31)
 
