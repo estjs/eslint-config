e73096a8a82799d3b292831ccd672b270ad1464b
jiangxd <jiangxd2016@gmail.com>
2023-08-03 10:10:43 +0800

    test: test singleQuote

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -175,7 +175,7 @@ module.exports = {
 		// Common
 		'semi': ['warn', 'always'],
 		'curly': ['error', 'all'],
-		'quotes': ['warn', 'single'],
+		'quotes': ['warn', 'single', { avoidEscape: true, allowTemplateLiterals: false }],
 		'quote-props': ['warn', 'consistent-as-needed'],
 		'no-unused-vars': 'warn',
 		'no-param-reassign': 'off',
