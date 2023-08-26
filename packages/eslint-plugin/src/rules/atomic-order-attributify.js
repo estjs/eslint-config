588e2e9765a3eab58e242ed138d10bf6b79ca780
jiangxd <jiangxd2016@gmail.com>
2023-08-26 22:18:47 +0800

    fix: fix attributify sort result

--- packages/eslint-plugin/src/rules/atomic-order-attributify.js
+++ packages/eslint-plugin/src/rules/atomic-order-attributify.js
@@ -1,6 +1,5 @@
 import { join } from 'node:path';
 import { createSyncFn } from 'synckit';
-import MagicString from 'magic-string';
 import { distDir } from '../utils/utils';
 
 const sortClasses = createSyncFn(join(distDir, 'workerSort.cjs'));
@@ -41,29 +40,13 @@ export default {
 					.map(i => i.key.name)
 					.join(' ')
 					.trim();
-				const sorted = sortClasses(input);
-				if (sorted !== input) {
+				const { isSorted, orderedClassNames } = sortClasses(input);
+				if (isSorted) {
 					context.report({
 						node,
 						messageId: 'invalid-order',
 						fix(fixer) {
-							const codeFull = context.getSourceCode();
-							const offset = node.range[0];
-							const code = codeFull.getText().slice(node.range[0], node.range[1]);
-
-							const s = new MagicString(code);
-
-							const sortedNodes = valueless
-								.map(i => [i.range[0] - offset, i.range[1] - offset])
-								.sort((a, b) => b[0] - a[0]);
-
-							for (const [start, end] of sortedNodes.slice(1)) {
-								s.remove(start, end);
-							}
-
-							s.overwrite(sortedNodes[0][0], sortedNodes[0][1], ` ${sorted.trim()} `);
-
-							return fixer.replaceText(node, s.toString());
+							return fixer.replaceText(node, `'${orderedClassNames}'`);
 						},
 					});
 				}
