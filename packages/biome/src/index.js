e9da01fdbf0cbbf8b04ade449564f95f0cf231c4
jiangxd <jiangxd2016@gmail.com>
2024-01-21 15:23:28 +0800

    feat(eslint-plugin-biome): test biome js api

--- packages/biome/src/index.js
+++ packages/biome/src/index.js
@@ -1,1 +1,11 @@
-export default {};
+import { Biome, Distribution } from '@biomejs/js-api';
+
+const biome = await Biome.create({
+	distribution: Distribution.NODE, // Or BUNDLER / WEB depending on the distribution package you've installed
+});
+
+const formatted = biome.formatContent('function f   (a, b) { return a == b; }', {
+	filePath: 'example.js',
+});
+
+console.log('Formatted content: ', formatted.content);
