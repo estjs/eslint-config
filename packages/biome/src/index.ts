1a0cbcea22f0befe72702835b47f420a40c59420
jiangxd <jiangxd2016@gmail.com>
2024-01-22 17:56:25 +0800

    fix: fix boime filePath

--- packages/biome/src/index.ts
+++ packages/biome/src/index.ts
@@ -3,24 +3,24 @@ import { Biome } from './biome';
 import { generateDifferences, showInvisibles } from './helpers';
 
 function reportDifference(context, difference) {
-  const { operation, offset, deleteText = '', insertText = '' } = difference;
-  const range = /** @type {Range} */ ([offset, offset + deleteText.length]);
-  // `context.getSourceCode()` was deprecated in ESLint v8.40.0 and replaced
-  // with the `sourceCode` property.
-  // TODO: Only use property when our eslint peerDependency is >=8.40.0.
-  const [start, end] = range.map(index =>
-    (context.sourceCode ?? context.getSourceCode()).getLocFromIndex(index),
-  );
+	const { operation, offset, deleteText = '', insertText = '' } = difference;
+	const range = /** @type {Range} */ ([offset, offset + deleteText.length]);
+	// `context.getSourceCode()` was deprecated in ESLint v8.40.0 and replaced
+	// with the `sourceCode` property.
+	// TODO: Only use property when our eslint peerDependency is >=8.40.0.
+	const [start, end] = range.map(index =>
+		(context.sourceCode ?? context.getSourceCode()).getLocFromIndex(index),
+	);
 
-  context.report({
-    messageId: operation,
-    data: {
-      deleteText: showInvisibles(deleteText),
-      insertText: showInvisibles(insertText),
-    },
-    loc: { start, end },
-    fix: fixer => fixer.replaceTextRange(range, insertText),
-  });
+	context.report({
+		messageId: operation,
+		data: {
+			deleteText: showInvisibles(deleteText),
+			insertText: showInvisibles(insertText),
+		},
+		loc: { start, end },
+		fix: fixer => fixer.replaceTextRange(range, insertText),
+	});
 }
 
 let biome;
@@ -34,23 +34,23 @@ export default {
 			recommended: 'warn',
 		},
 		messages: {
-			'invalid-biome': 'this content is not formatted with Biome',
+			'invalid-biome': 'this content is not content with Biome',
 		},
 		schema: [],
 	},
 	defaultOptions: [],
-	 create(context) {
+	create(context) {
 
 		const fileInfoOptions =
-		(context.options[1] && context.options[1].fileInfoOptions) || {};
+			(context.options[1] && context.options[1].fileInfoOptions) || {};
 
-	const sourceCode = context.sourceCode ?? context.getSourceCode();
+		const sourceCode = context.sourceCode ?? context.getSourceCode();
 
-	const filepath = context.filename ?? context.getFilename();
+		const filepath = context.filename ?? context.getFilename();
 
-	const onDiskFilepath =
-		context.physicalFilename ?? context.getPhysicalFilename();
-	const source = sourceCode.text;
+		const onDiskFilepath =
+			context.physicalFilename ?? context.getPhysicalFilename();
+		const source = sourceCode.text;
 
 		return {
 			async Program() {
@@ -58,13 +58,17 @@ export default {
 					biome = await Biome.create();
 					biome.applyConfiguration(BiomeConfig);
 				}
-				const formatted = biome.formatContent(source);
+				const { content } = biome.formatContent(source, {
+					...fileInfoOptions,
+					filepath,
+					onDiskFilepath,
+				});
 
-				if (formatted == null) {
+				if (content == null) {
 					return;
 				}
-				if (source !== formatted) {
-					const differences = generateDifferences(source, formatted);
+				if (source !== content) {
+					const differences = generateDifferences(source, content);
 
 					for (const difference of differences) {
 						reportDifference(context, difference);
