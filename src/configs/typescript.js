d8634b5dbc305c295c28f6a2e6fa3fbb45912fd8
jiangxd <jiangxd2016@gmail.com>
2024-02-24 17:58:18 +0800

    feat: off @typescript-eslint/no-dynamic-delete

--- src/configs/typescript.js
+++ src/configs/typescript.js
@@ -3,74 +3,76 @@ import { parserTypeScript, pluginTypeScript } from '../plugins';
 import { restrictedSyntaxJs } from './javascript';
 
 export function typescript(overrides = {}) {
-  return [
-    {
-      files: [GLOB_TS, GLOB_TSX],
-      languageOptions: {
-        parser: parserTypeScript,
-        parserOptions: {
-          sourceType: 'module',
-        },
-      },
-      plugins: {
-        '@typescript-eslint': pluginTypeScript,
-      },
-      rules: {
-        ...pluginTypeScript.configs['eslint-recommended'].overrides[0].rules,
-        ...pluginTypeScript.configs.strict.rules,
+	return [
+		{
+			files: [GLOB_TS, GLOB_TSX],
+			languageOptions: {
+				parser: parserTypeScript,
+				parserOptions: {
+					sourceType: 'module',
+				},
+			},
+			plugins: {
+				'@typescript-eslint': pluginTypeScript,
+			},
+			rules: {
+				...pluginTypeScript.configs['eslint-recommended'].overrides[0].rules,
+				...pluginTypeScript.configs.strict.rules,
 
-        '@typescript-eslint/ban-ts-comment': 'off',
-        '@typescript-eslint/ban-types': 'off',
-        '@typescript-eslint/consistent-type-imports': [
-          'error',
-          { disallowTypeAnnotations: false, fixStyle: 'inline-type-imports' },
-        ],
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-import-type-side-effects': 'error',
-        '@typescript-eslint/no-non-null-assertion': 'off',
-        '@typescript-eslint/no-redeclare': 'error',
+				'@typescript-eslint/ban-ts-comment': 'off',
+				'@typescript-eslint/ban-types': 'off',
+				'@typescript-eslint/consistent-type-imports': [
+					'error',
+					{ disallowTypeAnnotations: false, fixStyle: 'inline-type-imports' },
+				],
+				'@typescript-eslint/no-explicit-any': 'off',
+				'@typescript-eslint/no-import-type-side-effects': 'error',
+				'@typescript-eslint/no-non-null-assertion': 'off',
+				'@typescript-eslint/no-redeclare': 'error',
 
-        // handled by unused-imports/no-unused-imports
-        '@typescript-eslint/no-unused-vars': 'off',
+				'@typescript-eslint/no-dynamic-delete': 'off',
 
-        '@typescript-eslint/prefer-as-const': 'warn',
-        '@typescript-eslint/prefer-literal-enum-member': [
-          'error',
-          { allowBitwiseExpressions: true },
-        ],
+				// handled by unused-imports/no-unused-imports
+				'@typescript-eslint/no-unused-vars': 'off',
 
-        'no-restricted-syntax': ['error', ...restrictedSyntaxJs, 'TSEnumDeclaration[const=true]'],
+				'@typescript-eslint/prefer-as-const': 'warn',
+				'@typescript-eslint/prefer-literal-enum-member': [
+					'error',
+					{ allowBitwiseExpressions: true },
+				],
 
-        ...overrides,
-      },
-    },
-    {
-      files: ['**/*.d.ts'],
-      rules: {
-        'eslint-comments/no-unlimited-disable': 'off',
-        'import/no-duplicates': 'off',
-        'unused-imports/no-unused-vars': 'off',
-        ...overrides,
-      },
-    },
-    {
-      files: ['**/*.{test,spec}.ts?(x)'],
-      rules: {
-        'no-unused-expressions': 'off',
-      },
-    },
-    {
-      files: [GLOB_JS, '**/*.cjs'],
-      rules: {
-        '@typescript-eslint/no-require-imports': 'off',
-        '@typescript-eslint/no-var-requires': 'off',
-      },
-    },
-    {
-      files: ['**/*.d.ts'],
-      rules: {
-        'no-restricted-syntax': ['error', ...restrictedSyntaxJs],
-      },
-    },
-  ];
+				'no-restricted-syntax': ['error', ...restrictedSyntaxJs, 'TSEnumDeclaration[const=true]'],
+
+				...overrides,
+			},
+		},
+		{
+			files: ['**/*.d.ts'],
+			rules: {
+				'eslint-comments/no-unlimited-disable': 'off',
+				'import/no-duplicates': 'off',
+				'unused-imports/no-unused-vars': 'off',
+				...overrides,
+			},
+		},
+		{
+			files: ['**/*.{test,spec}.ts?(x)'],
+			rules: {
+				'no-unused-expressions': 'off',
+			},
+		},
+		{
+			files: [GLOB_JS, '**/*.cjs'],
+			rules: {
+				'@typescript-eslint/no-require-imports': 'off',
+				'@typescript-eslint/no-var-requires': 'off',
+			},
+		},
+		{
+			files: ['**/*.d.ts'],
+			rules: {
+				'no-restricted-syntax': ['error', ...restrictedSyntaxJs],
+			},
+		},
+	];
 }
