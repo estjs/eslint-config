d0117798ed8733d388d9ba4d8f8ed80ab2036004
jiangxd <jiangxd2016@gmail.com>
2025-01-12 19:55:10 +0800

    feat: format code

--- fixtures/output/no-style/jsx.jsx
+++ fixtures/output/no-style/jsx.jsx
@@ -9,7 +9,7 @@ export function HelloWorld({
   }
 
   // TODO: Don't use random in render
-  const num = Math.floor(Math.random() * 1e7)
+  let num = Math.floor(Math.random() * 1e7)
     .toString()
     .replaceAll(/\.\d+/g, '');
 
