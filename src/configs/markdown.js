2f3257dad2bba2d2aa72f8f88b4366dee0b1128e
jiangxd <jiangxd2016@gmail.com>
2025-05-06 14:53:02 +0800

    feat: off markdown node rules

--- src/configs/markdown.js
+++ src/configs/markdown.js
@@ -28,6 +28,7 @@ export function markdown(overrides = {}) {
         '@typescript-eslint/no-namespace': 'off',
         '@typescript-eslint/no-redeclare': 'off',
         '@typescript-eslint/no-require-imports': 'off',
+        '@typescript-eslint/no-unused-expressions': 'off',
         '@typescript-eslint/no-unused-vars': 'off',
         '@typescript-eslint/no-use-before-define': 'off',
         '@typescript-eslint/no-var-requires': 'off',
@@ -38,6 +39,9 @@ export function markdown(overrides = {}) {
         'no-undef': 'off',
         'no-unused-expressions': 'off',
         'no-unused-vars': 'off',
+
+        'node/prefer-global/buffer': 'off',
+        'node/prefer-global/process': 'off',
 
         'unused-imports/no-unused-imports': 'off',
         'unused-imports/no-unused-vars': 'off',
