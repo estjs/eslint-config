617407718246b4478b835e7f3e4d724d53b21197
jiangxd2016 <jiangxd2016@gmail.com>
2026-09-01 17:57:42 +0800

    test: update vitest config and fixture snapshots

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    include: ['test/**/*.test.{js,ts}'],
     testTimeout: 100000,
     globals: true,
     environment: 'node',
